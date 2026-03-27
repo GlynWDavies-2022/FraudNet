cae8d49c6ed64bb734b423e129e3b9eeae6b151e
Glyn Davies <glynwdavies@gmail.com>
2026-03-27 17:08:08 +0000

    Added additional unit test.

--- FraudNet.API.Tests/Entities/PaymentShould.cs
+++ FraudNet.API.Tests/Entities/PaymentShould.cs
@@ -5,7 +5,7 @@ namespace FraudNet.API.Tests.Entities;
 public class PaymentShould
 {
     [Fact]
-    public void ReturnTrueWhenTwoPaymentsAreIdentical()
+    public void ReturnTrueWhenTwoPaymentsAreEqual()
     {
         // Arrange
 
@@ -38,5 +38,41 @@ public class PaymentShould
         // Assert
 
         Assert.True(result);
+    }
+
+    [Fact]
+    public void ReturnFalseWhenTwoPaymentsAreNotEqual()
+    {
+        // Arrange
+
+        var payment = new Payment
+        {
+            Id = 1,
+            Timestamp = new DateTime(2026, 3, 27, 1, 0, 0),
+            Amount = 999.99M,
+            Reference = "ABC-12345",
+            PayeeId = 1,
+            Type = PaymentType.REGULAR,
+            BatchId = 1,
+        };
+
+        var duplicate = new Payment
+        {
+            Id = 2,
+            Timestamp = new DateTime(2026, 3, 27, 3, 0, 0),
+            Amount = 999.99M,
+            Reference = "ABC-12340",
+            PayeeId = 2,
+            Type = PaymentType.REGULAR,
+            BatchId = 1,
+        };
+
+        // Act
+
+        var result = payment.Equals(duplicate);
+
+        // Assert
+
+        Assert.False(result);
     }
 }
