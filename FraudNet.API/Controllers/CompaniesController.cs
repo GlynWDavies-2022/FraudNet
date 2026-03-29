6e58308cbe620e9601809d7ea780172c44ae013c
Glyn Davies <glynwdavies@gmail.com>
2026-03-29 18:53:38 +0100

    Added additional controllers.

---
+++ FraudNet.API/Controllers/CompaniesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FraudNet.API.Controllers;
+
+[Route("api/companies")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+}
---
+++ FraudNet.API/Controllers/PayeesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FraudNet.API.Controllers;
+
+[Route("api/payees")]
+[ApiController]
+public class PayeesController : ControllerBase
+{
+}
